eb7d2b1ddf1e5301112e9a42c59f10479418a6be
Zach Lipp <zach.a.lipp@gmail.com>
2019-11-09 21:31:26 -0600

    fully functioning filters

--- main.py
+++ main.py
@@ -82,10 +82,10 @@ if __name__ == "__main__":
     )
 
     @app.callback(Output("search", "data"), [Input("search", "filter_query")])
-    def update_nodes(filter):
+    def update_names(filter, id_field="Name"):
         id_field = "Name"
         filtering_expressions = filter.split(" && ")
-        dff = nodes[[id_field]]
+        dff = nodes[[id_field]].sort_values(id_field)
         for filter_part in filtering_expressions:
             col_name, operator, filter_value = split_filter_part(filter_part)
             if operator in ("eq", "ne", "lt", "le", "gt", "ge"):
@@ -99,22 +99,50 @@ if __name__ == "__main__":
                 dff = dff.loc[dff[col_name].str.startswith(filter_value)]
         return dff.to_dict("records")
 
-    @app.callback(Output("source", "data"), [Input("search", "filter_query")])
-    def update_nodes(filter):
-        id_field = "Name"
-        filtering_expressions = filter.split(" && ")
-        dff = nodes[[id_field]]
-        for filter_part in filtering_expressions:
-            col_name, operator, filter_value = split_filter_part(filter_part)
-            if operator in ("eq", "ne", "lt", "le", "gt", "ge"):
-                # these operators match pandas series operator method names
-                dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
-            elif operator == "contains":
-                dff = dff.loc[dff[col_name].str.contains(filter_value)]
-            elif operator == "datestartswith":
-                # this is a simplification of the front-end filtering logic,
-                # only works with complete fields in standard format
-                dff = dff.loc[dff[col_name].str.startswith(filter_value)]
-        return nodes[nodes[id_field].isin(dff[id_field])].to_dict("records")
+    @app.callback(Output("source", "data"), [Input("search", "active_cell")])
+    def update_sources(
+        selection,
+        id_field="Name",
+        target_field="Target",
+        source_field="Source",
+    ):
+        columns = ["Name", "Historical Significance", "Gender"]
+        sorted_names = nodes[id_field].sort_values().tolist()
+        if selection:
+            # Get occurrences of name in the source file
+            row_index = selection["row"]
+            person_id = sorted_names[row_index]
+
+            # The people this person sourced
+            source_names = edges[edges[source_field] == person_id][
+                target_field
+            ].tolist()
+            sources = nodes[nodes[id_field].isin(source_names)]
+            return sources[columns].to_dict("records")
+        else:
+            return nodes[columns].to_dict("records")
+
+    @app.callback(Output("target", "data"), [Input("search", "active_cell")])
+    def update_targets(
+        selection,
+        id_field="Name",
+        target_field="Target",
+        source_field="Source",
+    ):
+        columns = ["Name", "Historical Significance", "Gender"]
+        sorted_names = nodes[id_field].sort_values().tolist()
+        if selection:
+            # Get occurrences of name in the source file
+            row_index = selection["row"]
+            person_id = sorted_names[row_index]
+
+            # The people who sourced this person
+            target_names = edges[edges[target_field] == person_id][
+                source_field
+            ].tolist()
+            targets = nodes[nodes[id_field].isin(target_names)]
+            return targets[columns].to_dict("records")
+        else:
+            return nodes[columns].to_dict("records")
 
     app.run_server(host="0.0.0.0", debug=True)
